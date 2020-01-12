ed70b0ffa6b4ee8e43405e7bdd5c4cd01c698c49
Denys Meloshyn <denys.meloshyn@gmail.com>
2020-01-12 20:04:10 +0100

    sonar: Test

--- ShareBudget/Presenter/BasePresenter.swift
+++ ShareBudget/Presenter/BasePresenter.swift
@@ -16,6 +16,7 @@ protocol BasePresenterDelegate: class {
 }
 
 protocol BasePresenterProtocol: LifeCycleStateProtocol {
+    
 }
 
 class BasePresenter<Interaction: BaseInteractionProtocol, Router: BaseRouterProtocol>: NSObject {
